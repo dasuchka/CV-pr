896b30a0ac586db6e9b51753166aea0cda9a5248
dasuchka <70776259+dasuchka@users.noreply.github.com>
2023-03-07 12:24:01 +0300

    make some changes in js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert(navigator.userAgent, navigator.platform, navigator.language, navigator.appName, navigator.appVersion, navigator.geolocation )
+var inf=navigator.userAgent+'\n' +navigator.platform+'\n' +navigator.language+'\n' +navigator.appName+'\n' +navigator.appVersion+ '\n' +navigator.geolocation 
+alert(inf)
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  document.getElementById("demo").innerHTML =
+  "Geolocation is not supported by this browser.";
+}
+
+function showPosition(position) {
+  document.getElementById("demo").innerHTML =
+  "Latitude: " + position.coords.latitude +
+  "Longitude: " + position.coords.longitude;
+}
+
+alert(geo_inf)
